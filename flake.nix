30b4cc42a3fbb77bcdb32112c7caddd22e76d008
tom-blk <tom.bleek@proton.me>
2024-05-06 16:09:14 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+    description = "Nix Config Flake";
+
+    inputs = {
+        nixpkgs.url = "nixpkgs/nixos-unstable";
+        home-manager.url = "github:nix-community/home-manager/master";
+        home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    outputs = { self, nixpkgs, home-manager, ... }: 
+        let 
+            system = "x86_64-linux";
+            lib = nixpkgs.lib;
+            pkgs = nixpkgs.legacyPackages.${system};
+        in{
+            nixosConfigurations = {
+                tom-b-nixos = lib.nixosSystem{
+                    inherit system;
+                    modules = [ ./configuration.nix ];
+                };
+            };
+            homeConfigurations = {
+                tom = home-manager.lib.homeManagerConfiguration{
+                    inherit pkgs;
+                    modules = [ ./home.nix ];
+                };
+            };   
+        };
+}
+
