417aa066332255dedf6fe15cc651ea30133a6475
tom-blk <tom.bleek@proton.me>
2024-05-09 08:20:29 +0200

    Added jack

--- home.nix
+++ home.nix
@@ -22,7 +22,9 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    discord
+  jack2
+	slack
+	discord
     guitarix
     reaper
   	brave
